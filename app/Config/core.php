8841bbfa481ebcf45e73406831ebae4d06d49dcc
Yannick Lohse <glorf.travian@gmail.com>
2013-01-19 19:09:18 +0100

    Fin de la migration

--- app/Config/core.php
+++ app/Config/core.php
@@ -186,12 +186,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'dgf46d84gsg48zsGGGE48948sg984sg');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '4489409848409840894840890980');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
